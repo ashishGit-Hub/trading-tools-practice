a5a78aaa780671dd2e2c6d4a3d71690d97959eda
Ashish <sb478590@gmail.com>
2022-07-20 06:27:52 +0530

    add Navigation Graph

---
+++ app/src/main/java/com/unboxtechnicallife/tradingtoolspractice/AddJournalFragment.kt
@@ -0,0 +1,20 @@
+package com.unboxtechnicallife.tradingtoolspractice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddJournalFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_journal, container, false)
+    }
+
+}
